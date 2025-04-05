925efc4a65e44895fa180852e6aae2f8f6f69018
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-05 16:32:54 +0300

    Initial commit

--- frontend/src/pages/Dictionary.tsx
+++ frontend/src/pages/Dictionary.tsx
@@ -1,1 +1,7 @@
-// TODO: Реализовать страницу словаря
+export default function Dictionary() {
+	return (
+		<div className="p-4">
+			<h1 className="text-xl text-white">Dictionary Page (Soon)</h1>
+		</div>
+	)
+}// TODO: Реализовать страницу словаря
--- frontend/src/pages/WordDetail.tsx
+++ frontend/src/pages/WordDetail.tsx
@@ -1,1 +1,7 @@
-// TODO: Реализовать страницу деталей слова
+export default function WordDetail() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl text-white">Word Detail Page (Soon)</h1>
+    </div>
+  )
+}
