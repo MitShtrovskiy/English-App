791e6250b41c3a9c108f87bfc6c960b0f0d02b92
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-05 17:09:38 +0300

    Initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import words
+from app.routers import words
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://english-app-nine.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
