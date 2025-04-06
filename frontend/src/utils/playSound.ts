40dc5b7b9a218a2743a5a62e27a7abc9947668df
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-06 22:44:13 +0300

    полностью переношу стили 2

---
+++ frontend/src/utils/playSound.ts
@@ -0,0 +1,7 @@
+// src/utils/playSound.ts
+
+export const playSound = (text: string) => {
+  const utterance = new SpeechSynthesisUtterance(text)
+  utterance.lang = 'en-US'
+  window.speechSynthesis.speak(utterance)
+}
