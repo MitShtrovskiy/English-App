fa20300996b2326cfcd69f951ebff73ee015c664
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-08 01:33:50 +0300

    Делаю, чтобы всё сохранялось

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,10 +12,7 @@ app = FastAPI()
 # CORS: разрешаем фронту обращаться к API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://english-app-nine.vercel.app"  # ← сюда вставь свой домен с Vercel
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
