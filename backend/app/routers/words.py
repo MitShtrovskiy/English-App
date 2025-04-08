0951e51bd5c996319e4efe4063299e2e2b1a84ca
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-08 23:11:30 +0300

    правлю бэк

--- backend/app/routers/words.py
+++ backend/app/routers/words.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
 from sqlalchemy.orm import Session
 import csv, codecs
 from database import SessionLocal, engine
-from app import schemas
+from app import models, schemas
 
 models.Base.metadata.create_all(bind=engine)
 
