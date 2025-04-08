c96159712c8876a74f4e16886da1140375b18e81
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-08 20:08:27 +0300

    Добавляю кнопку

--- frontend/src/pages/WordListPage.tsx
+++ frontend/src/pages/WordListPage.tsx
@@ -103,6 +103,17 @@ export default function WordListPage() {
           ))}
         </div>
       </div>
+      
+      {/* ➕ Кнопка добавления слова */}
+      <div className="sticky bottom-0 bg-black w-full px-5 pb-6 pt-3">
+        <button
+          onClick={() => navigate('/upload')}
+          className="w-full h-14 rounded-[20px] bg-white/10 active:bg-white/20 text-white text-[16px] font-medium"
+        >
+          Добавить слово
+        </button>
+      </div>
+      
     </div>
   )
 }
