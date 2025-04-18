130761fc6f1caaaf2ad0c051ed23dced542bda88
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-18 22:56:24 +0300

    правлю загруженную базу

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,9 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# 🧠 Определяем путь к файлу базы
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, '../words.db')}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
