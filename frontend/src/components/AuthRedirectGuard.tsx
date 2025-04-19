6c2f8aebf68047092437d995a9941f84819fcfa7
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-19 22:31:21 +0300

    мутим авторизацию / регистрацию

--- frontend/src/components/AuthRedirectGuard.tsx
+++ frontend/src/components/AuthRedirectGuard.tsx
@@ -1,13 +1,12 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '@/context/AuthContext'
 
-
-export default function AuthRedirectGuard({ component }: { component: JSX.Element }) {
+export default function AuthRedirectGuard({ children }: { children: JSX.Element }) {
   const { token } = useAuth()
 
   if (token) {
     return <Navigate to="/" />
   }
 
-  return component
+  return children
 }
