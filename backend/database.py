71b33c1844259477f6bd6cd302cf21df9dce5b74
MitShtrovskiy <34007597+MitShtrovskiy@users.noreply.github.com>
2025-04-20 19:14:34 +0300

    подключаем нестираемую базу

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,15 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./words.db"
+# Используем переменные окружения
+from dotenv import load_dotenv
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
